d4ec38980e324a8a6dbad08955f84d04de87d342
kiwan33 <rumansillar@gmail.com>
2025-02-25 19:09:36 +0100

    Add login and register

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import './App.css'
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import Template from './components/template/Template';
+import Login from './components/login/Login';
 import Home from './components/home/Home';
+import AuthPage  from './components/auth/AuthPage'; 
 
 function App() {
   return (
     <Routes> {/* Define el contenedor de rutas */}
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<AuthPage />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/template" element={<Template />} />
       </Routes>
   );
